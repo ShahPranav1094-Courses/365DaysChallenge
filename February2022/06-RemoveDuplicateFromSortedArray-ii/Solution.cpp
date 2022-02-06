3c7563b5c01c56ad8bcb0638c0971e2cc5d29753
shahpranav1094 <shahpranav1094@gmail.com>
2022-02-06 11:46:42 +0530

    [06-RemoveDuplicateFromSortedArray] Optimized

---
+++ February2022/06-RemoveDuplicateFromSortedArray-ii/Solution.cpp
@@ -0,0 +1,10 @@
+// Optimized
+// O(N) Time & O(1) Space
+int removeDuplicates(vector<int> &nums)
+{
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i - 2])
+            nums[i++] = n;
+    return i;
+}
