a351c19004057098faec268e68d31b6177d4ba9f
Pranav Shah4 <pshah4@lenovo.com>
2022-03-22 21:58:18 +0530

    [22] SmallestStringWithAGivenNumericValue

---
+++ March2022/22-SmallestStringWithAGivenNumericValue/Solution.cpp
@@ -0,0 +1,22 @@
+// O(N) Time & O(1) Space
+string getSmallestString(int n, int k)
+{
+    string s(n, 'a');
+    k -= n;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (k == 0)
+            break;
+        if (k < 25)
+        {
+            s[i] = char('a' + k);
+            k = 0;
+        }
+        else
+        {
+            s[i] = char('a' + 25);
+            k -= 25;
+        }
+    }
+    return s;
+}
